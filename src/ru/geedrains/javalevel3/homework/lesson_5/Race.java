96ca57fcd93444593e99e154b8d1481602ca3d5f
Iam_Student_Geekrains <temaivan@mail.ru>
2020-10-12 16:35:31 +0400

    Initial commit

---
+++ src/ru/geedrains/javalevel3/homework/lesson_5/Race.java
@@ -0,0 +1,16 @@
+package ru.geedrains.javalevel3.homework.lesson_5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
---
+++ src/ru/geedrains/javalevel3/homework/lesson_5/Tunnel.java
@@ -0,0 +1,24 @@
+package ru.geedrains.javalevel3.homework.lesson_5;
+
+public class Tunnel extends Stage {
+    public Tunnel(int i) {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
